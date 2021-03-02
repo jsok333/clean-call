938005f7d7f50639922eb391395f60933da98995
jsok333 <jsok333@gmail.com>
2021-03-03 03:49:09 +0900

    0303 ok1

--- cleancall/src/main/java/cleancall/external/CleanmanageServiceFallback.java
+++ cleancall/src/main/java/cleancall/external/CleanmanageServiceFallback.java
@@ -5,13 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class CleanmanageServiceFallback implements CleanmanageService {
 	 
-	//@Override
-	//public void 택시할당요청(Cleanmanage Cleanmanage)
-	//{	
-	//	System.out.println("Circuit breaker has been opened. Fallback returned instead.");
-	//}
-	
-	
 	@Override
 	public void cleanManageCall( Cleanmanage cleanmanage) {
 		// TODO Auto-generated method stub
